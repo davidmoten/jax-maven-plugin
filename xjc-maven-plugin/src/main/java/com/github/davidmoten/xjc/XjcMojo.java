829003be48efd79bcea10fe25b8199661a5d780b
Dave Moten <davidmoten@gmail.com>
2019-01-09 14:48:16 +1100

    XjcMojo dev

--- xjc-maven-plugin/src/main/java/com/github/davidmoten/xjc/XjcMojo.java
+++ xjc-maven-plugin/src/main/java/com/github/davidmoten/xjc/XjcMojo.java
@@ -2,6 +2,7 @@ package com.github.davidmoten.xjc;
 
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -11,9 +12,9 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "xjc")
-public class XjcMojo extends AbstractMojo {
+public final class XjcMojo extends AbstractMojo {
 
-    @Parameter(property = "argument")
+    @Parameter(required = true, name = "arguments")
     private List<String> arguments;
 
     @Override
@@ -21,17 +22,24 @@ public class XjcMojo extends AbstractMojo {
 
         Log log = getLog();
         log.info("Starting xjc mojo");
-        
+
         ensureDestinationDirectoryExists();
-        
-        
+
+        try {
+            log.info("running com.sun.tools.xjc.Driver.run method with arguments:");
+            log.info(arguments.stream().collect(Collectors.joining(" ")));
+            com.sun.tools.xjc.Driver.run(arguments.toArray(new String[] {}), System.out, System.out);
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+        log.info("xjc mojo finished");
     }
 
     private void ensureDestinationDirectoryExists() {
         Log log = getLog();
-        for (int i = 0;i < arguments.size();i++) {
+        for (int i = 0; i < arguments.size(); i++) {
             if (arguments.get(i).trim().equals("-d") && i < arguments.size() - 1) {
-                File dir = new File(arguments.get(i+1));
+                File dir = new File(arguments.get(i + 1));
                 if (!dir.exists()) {
                     log.info("destination directory (-d option) specified and does not exist, creating: " + dir);
                     dir.mkdirs();
