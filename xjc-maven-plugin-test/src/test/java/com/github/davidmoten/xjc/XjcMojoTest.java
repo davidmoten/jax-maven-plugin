d3dbfab59c1d672040188009ddab14511ca45734
Dave Moten <davidmoten@gmail.com>
2019-01-09 16:37:14 +1100

    cleanup

--- xjc-maven-plugin-test/src/test/java/com/github/davidmoten/xjc/XjcMojoTest.java
+++ xjc-maven-plugin-test/src/test/java/com/github/davidmoten/xjc/XjcMojoTest.java
@@ -3,12 +3,16 @@ package com.github.davidmoten.xjc;
 import org.junit.Test;
 
 import dummy.ObjectFactory;
+import dummy.Param;
+import dummy.Record;
 
 public class XjcMojoTest {
     
     @Test
     public void testClassesGeneratedFromDtd() {
        System.out.println("Found generated class " + ObjectFactory.class.getName());
+       System.out.println("Found generated class " + Param.class.getName());
+       System.out.println("Found generated class " + Record.class.getName());
     }
 
 }
--- xjc-maven-plugin/src/main/java/com/github/davidmoten/xjc/XjcMojo.java
+++ xjc-maven-plugin/src/main/java/com/github/davidmoten/xjc/XjcMojo.java
@@ -26,36 +26,48 @@ public final class XjcMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
 
         Log log = getLog();
+
         log.info("Starting xjc mojo");
 
+        setSystemProperties();
+
+        ensureDestinationDirectoryExists();
+
+        callXjc();
+
+        log.info("xjc mojo finished");
+    }
+
+    private void callXjc() throws MojoExecutionException {
+        try {
+            getLog().info("running com.sun.tools.xjc.Driver.run method with arguments:\n" //
+                    + arguments //
+                            .stream() //
+                            .map(x -> "  " + x) //
+                            .collect(Collectors.joining("\n")));
+            if (com.sun.tools.xjc.Driver.run(arguments.toArray(new String[] {}), System.out, System.out) != 0) {
+                throw new MojoExecutionException("xjc failed, see log above for details");
+            }
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
+
+    private void setSystemProperties() {
         if (systemProperties != null) {
-            log.info("setting system properties: " + systemProperties);
+            getLog().info("setting system properties: " + systemProperties);
             for (Entry<String, String> entry : systemProperties.entrySet()) {
                 System.setProperty(entry.getKey(), entry.getValue());
             }
         }
-
-        ensureDestinationDirectoryExists();
-
-        try {
-            log.info("running com.sun.tools.xjc.Driver.run method with arguments:\n"
-                    + arguments.stream().map(x -> "  " + x).collect(Collectors.joining("\n")));
-            if (com.sun.tools.xjc.Driver.run(arguments.toArray(new String[] {}), System.out, System.out)!= 0) {
-                throw new MojoExecutionException("xjc failed, see log above for details");
-            };
-        } catch (Exception e) {
-            throw new MojoExecutionException(e.getMessage(), e);
-        }
-        log.info("xjc mojo finished");
     }
 
     private void ensureDestinationDirectoryExists() {
-        Log log = getLog();
         for (int i = 0; i < arguments.size(); i++) {
             if (arguments.get(i).trim().equals("-d") && i < arguments.size() - 1) {
                 File dir = new File(arguments.get(i + 1));
                 if (!dir.exists()) {
-                    log.info("destination directory (-d option) specified and does not exist, creating: " + dir);
+                    getLog().info("destination directory (-d option) specified and does not exist, creating: " + dir);
                     dir.mkdirs();
                 }
             }
