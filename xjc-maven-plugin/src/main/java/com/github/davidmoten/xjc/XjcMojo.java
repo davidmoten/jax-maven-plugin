35c1c0c309deed74c3bcde5cba2afbdfe2f84c9a
Dave Moten <davidmoten@gmail.com>
2019-01-11 10:46:29 +1100

    explicitly build classpath

--- xjc-maven-plugin/src/main/java/com/github/davidmoten/xjc/XjcMojo.java
+++ xjc-maven-plugin/src/main/java/com/github/davidmoten/xjc/XjcMojo.java
@@ -1,9 +1,14 @@
 package com.github.davidmoten.xjc;
 
 import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -12,6 +17,8 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+
+import com.google.common.collect.Sets;
 
 @Mojo(name = "xjc")
 public final class XjcMojo extends AbstractMojo {
@@ -27,6 +34,56 @@ public final class XjcMojo extends AbstractMojo {
 
         Log log = getLog();
 
+        // Artifact a = project //
+        // .getArtifacts() //
+        // .stream() //
+        // .filter(x -> x.getGroupId().equals("org.glassfish.jaxb") &&
+        // x.getArtifactId().equals("jaxb-xjc")) //
+        // .findFirst() //
+        // .get();
+        //
+
+        // System.out.println("dependencyTrail=" + a.getDependencyTrail());
+
+        String v = "-2.4.0-b180830.0438.jar";
+        Set<String> filenames = Sets.newHashSet("jaxb-xjc" + v, //
+                "jaxb-runtime" + v, //
+                "xsom" + v, //
+                "relaxng-datatype" + v, //
+                "codemodel" + v, //
+                "dtd-parser-1.4.jar", //
+                "istack-commons-tools-3.0.7.jar", //
+                "ant-1.10.2.jar", //
+                "ant-launcher-1.10.2.jar", //
+                "istack-commons-runtime-3.0.7.jar", //
+                "jaxb-api-2.4.0-b180830.0359.jar", //
+                "javax.activation-api-1.2.0.jar", //
+                "txw2" + v, //
+                "stax-ex-1.8.jar", //
+                "FastInfoset-1.2.15.jar" //
+        );
+        final URLClassLoader classLoader = (URLClassLoader) Thread.currentThread().getContextClassLoader();
+        StringBuilder classpath = new StringBuilder();
+        Set<String> found = Sets.newHashSet();
+        for (final URL url : classLoader.getURLs()) {
+            File file = new File(url.getFile());
+            log.info("classpath entry: " + file.getName());
+            if (filenames.contains(file.getName())) {
+                found.add(file.getName());
+                if (classpath.length() > 0) {
+                    classpath.append(File.pathSeparator);
+                }
+                classpath.append(file.getAbsolutePath());
+            }
+        }
+        if ( filenames.size() != found.size()) {
+            Set<String> set = new HashSet<String>(filenames);
+            set.removeAll(found);
+            throw new MojoExecutionException(
+                    "did not find all expected classpath. Missing " + set);
+        }
+        log.info("classpath=" + classpath);
+
         log.info("Starting xjc mojo");
 
         setSystemProperties();
@@ -37,7 +94,7 @@ public final class XjcMojo extends AbstractMojo {
 
         log.info("xjc mojo finished");
     }
-    
+
     private void ensureDestinationDirectoryExists() {
         for (int i = 0; i < arguments.size(); i++) {
             if (arguments.get(i).trim().equals("-d") && i < arguments.size() - 1) {
@@ -49,7 +106,7 @@ public final class XjcMojo extends AbstractMojo {
             }
         }
     }
-    
+
     private void setSystemProperties() {
         if (systemProperties != null) {
             getLog().info("setting system properties: " + systemProperties);
