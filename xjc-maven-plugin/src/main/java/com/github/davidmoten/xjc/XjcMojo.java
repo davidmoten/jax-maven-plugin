ac5df661324377b8fef08704498c902f6ccc3ec7
Dave Moten <davidmoten@gmail.com>
2019-01-11 14:22:43 +1100

    use Maven to resolve deps of jaxb-xjc for classpath restriction (was manual before) #1

--- xjc-maven-plugin/src/main/java/com/github/davidmoten/xjc/XjcMojo.java
+++ xjc-maven-plugin/src/main/java/com/github/davidmoten/xjc/XjcMojo.java
@@ -10,12 +10,19 @@ import java.util.Set;
 import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
+import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.repository.RepositorySystem;
 import org.zeroturnaround.exec.InvalidExitValueException;
 import org.zeroturnaround.exec.ProcessExecutor;
 
@@ -30,6 +37,12 @@ public final class XjcMojo extends AbstractMojo {
 
     @Parameter(name = "systemProperties")
     private Map<String, String> systemProperties;
+
+    @Component
+    private RepositorySystem repositorySystem;
+
+    @Parameter(defaultValue = "${localRepository}", readonly = true, required = true)
+    private ArtifactRepository localRepository;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -54,6 +67,8 @@ public final class XjcMojo extends AbstractMojo {
     }
 
     private List<String> createCommand() {
+
+        // https://stackoverflow.com/questions/1440224/how-can-i-download-maven-artifacts-within-a-plugin
 
         // get the current classpath (plugin classpath) and just hang on to the deps
         // required for jaxb-xjc
