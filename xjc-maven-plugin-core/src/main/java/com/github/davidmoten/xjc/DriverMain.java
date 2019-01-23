76fb0072eac7f38ecf02a1ae55a0f0ee9f6cd521
Dave Moten <davidmoten@gmail.com>
2019-01-24 09:44:18 +1100

    update README.md with xjc extension docs #2

--- xjc-maven-plugin-core/src/main/java/com/github/davidmoten/xjc/DriverMain.java
+++ xjc-maven-plugin-core/src/main/java/com/github/davidmoten/xjc/DriverMain.java
@@ -4,8 +4,6 @@ import com.sun.tools.xjc.Driver;
 
 public class DriverMain {
 
-    private static final boolean USE_XJC_FACADE = true;
-
     public static void main(String[] args) throws Throwable {
         if (Driver.run(args, System.out, System.out) != 0) {
             throw new Exception("xjc call failed, see logs above for details");
