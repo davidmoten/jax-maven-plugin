1cceb9242d3f2945f9b5929166a6f056ab1c639d
Dave Moten <davidmoten@gmail.com>
2019-01-24 15:39:48 +1100

    jaxws-maven-plugin dev

--- jaxws-maven-plugin-test/src/test/java/com/github/davidmoten/jaxws/JaxwsMavenPluginTest.java
+++ jaxws-maven-plugin-test/src/test/java/com/github/davidmoten/jaxws/JaxwsMavenPluginTest.java
@@ -9,9 +9,14 @@ import org.junit.Test;
 public class JaxwsMavenPluginTest {
     
     @Test
-    public void testWsImportGeneratedFiles() {
+    public void testWsImportGeneratedFilesExist() {
         assertTrue(new File("target/generated-sources/jaxws/com/examples/wsdl/helloservice/HelloService.java").exists());
         assertTrue(new File("target/generated-classes/jaxws/com/examples/wsdl/helloservice/HelloService.class").exists());
+    }
+    
+    @Test
+    public void testGeneratedSourcesOnClasspath() throws ClassNotFoundException {
+        Class.forName("com.examples.wsdl.helloservice.HelloService");
     }
 
 }
