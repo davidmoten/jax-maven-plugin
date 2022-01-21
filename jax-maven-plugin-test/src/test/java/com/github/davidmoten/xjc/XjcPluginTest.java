5efe80068b1ed11bf2ea98f8993dc1e377422598
Dave Moten <davidmoten@gmail.com>
2022-01-21 15:31:10 +1100

    comment out plugin use because jaxb2-basics does not support jakarta yet

--- jax-maven-plugin-test/src/test/java/com/github/davidmoten/xjc/XjcPluginTest.java
+++ jax-maven-plugin-test/src/test/java/com/github/davidmoten/xjc/XjcPluginTest.java
@@ -2,8 +2,7 @@ package com.github.davidmoten.xjc;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.lang.reflect.Method;
-
+import org.junit.Ignore;
 import org.junit.Test;
 
 import dummy.ObjectFactory;
@@ -20,11 +19,12 @@ public class XjcPluginTest {
     }
 
     @Test
+    @Ignore // not supported for jakarta because jaxb2-basics (jaxb plugins) doesn't support jakarta yet
     public void testJaxb2PluginGeneratedCodePresent() {
-        assertNotNull(getDeclaredMethod(plugin.support.Param.class, "toString"));
-        assertNotNull(getDeclaredMethod(plugin.support.Param.class, "equals"));
-        assertNotNull(getDeclaredMethod(plugin.support.Param.class, "hashCode"));
-        System.out.println("Jaxb2-basics plugins worked correctly");
+//        assertNotNull(getDeclaredMethod(plugin.support.Param.class, "toString"));
+//        assertNotNull(getDeclaredMethod(plugin.support.Param.class, "equals"));
+//        assertNotNull(getDeclaredMethod(plugin.support.Param.class, "hashCode"));
+//        System.out.println("Jaxb2-basics plugins worked correctly");
     }
 
     @Test
@@ -33,14 +33,14 @@ public class XjcPluginTest {
         System.out.println("episode file generated correctly");
     }
 
-    private Method getDeclaredMethod(Class<?> clazz, String methodName)
-    {
-        for (Method method : clazz.getDeclaredMethods()) {
-          if(method.getName().equals(methodName)) {
-            return method;
-          }
-        }
-
-        throw new RuntimeException(new NoSuchMethodException(methodName));
-    }
+//    private Method getDeclaredMethod(Class<?> clazz, String methodName)
+//    {
+//        for (Method method : clazz.getDeclaredMethods()) {
+//          if(method.getName().equals(methodName)) {
+//            return method;
+//          }
+//        }
+//
+//        throw new RuntimeException(new NoSuchMethodException(methodName));
+//    }
 }
